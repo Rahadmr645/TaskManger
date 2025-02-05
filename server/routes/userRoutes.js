3916c4140b08f155c108c7542541ac90ce7c7f80
Rahad <rahadmr645@gmail.com>
2025-02-05 04:12:26 +0300

    complete first section in backend

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,94 @@
+import express from 'express'
+import User from '../models/userModels.js';
+const router = express.Router();
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+// 01 : create user
+
+router.post('/create', async (req, res) => {
+
+    try {
+
+        // import in body 
+        const { name, email, password } = req.body;
+
+
+        // check input is filled or not 
+        if (!name || !email || !password) {
+            return res.status(400).json({ success: false, message: "Fill all the input" });
+        }
+
+        // chack is email is exist or not 
+        const existUser = await User.findOne({ email })
+        if (existUser) return res.status(400).json({ success: false, message: "User is already exist" });
+
+        // make password hassing
+        const salt = await bcrypt.genSalt(10);
+        const hassPass = await bcrypt.hash(password, salt);
+
+
+        const newUser = new User({ name, email, password: hassPass });
+        await newUser.save();
+
+        res.status(200).json({ success: true, message: "User create successfully", user: newUser });
+
+    } catch (error) {
+        res.status(400).json({ success: false, message: "User create Faild", });
+        console.error(error);
+    }
+
+})
+
+
+// 02 : login user 
+
+router.post('/login', async (req, res) => {
+
+    try {
+
+        // import in body 
+        const { email, password } = req.body;
+
+
+        // check input is filled or not 
+        if (!email || !password) {
+            return res.status(400).json({ success: false, message: "Fill all the input" });
+        }
+
+        // chack is email is exist or not 
+        const existUser = await User.findOne({ email })
+        if (!existUser) return res.status(400).json({ success: false, message: "UnAuthorised cradintial" });
+
+        // compare hashing password
+        const comparePass = await bcrypt.compare(password, existUser.password);
+        if (!comparePass) return res.status(400).json({ success: false, message: "UnAuthorised cradintial" });
+
+        res.status(200).json({ success: true, message: `WellCome Dear ${existUser.name}` });
+
+    } catch (error) {
+        res.status(400).json({ success: false, message: "Login faild", });
+        console.error(error);
+    }
+
+})
+
+// 03 : Delete users
+
+router.post('/delete', async (req, res) => {
+    try {
+
+        // import in body 
+        const { id } = req.body;
+       
+        const deleteUser = await User.findByIdAndDelete( id );
+        res.status(200).json({ success: true, message: `  Dear ${deleteUser.name} your account Deleted Successfully` });
+    } catch (error) {
+        res.status(400).json({ success: false, message: " Faild to delete", });
+        console.error(error);
+    }
+
+})
+
+
+
+export default router;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import connectToMongoDb from './config/db.js';
-
+import userRoutes from './routes/userRoutes.js'
 const app = express();
 
 const PORT = 8001;
@@ -9,6 +9,8 @@ app.use(express.json());
 
 connectToMongoDb();
 
+// routes section
+app.use('/api/user/', userRoutes)
 app.get('/', (req, res) => {
     res.send('Hello Rahad')
 });
