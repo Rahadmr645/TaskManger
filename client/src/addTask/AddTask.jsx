553c52a3f69ecf3c7d2d7b4fed9bfbca1d33b067
Rahadmr645 <rahadmr645@gmail.com>
2025-02-08 01:29:02 +0300

    improve some functionality

--- client/src/addTask/AddTask.jsx
+++ client/src/addTask/AddTask.jsx
@@ -7,10 +7,40 @@ const AddTask = () => {
             <form className={styles.taskForm} action="">
                 <h1>Task Entry Form</h1>
                 <hr />
+                <div>
+                   <label htmlFor="project" >Project</label>
+                <select>
+                  <option>
+                    Important
+                  </option>
+                  <option>
+                    Corporate
+                  </option>
+                   <option>
+                    Corporate
+                  </option>
+                   <option>
+                    Corporate
+                  </option>
+                </select>
+                </div>
+               <div>
+                  <label htmlFor="task">Task</label>
                 <input type="text" />
-                <input type="text" />
-                <input type="text" />
-                <input type="text" />
+               </div>
+                <div>
+               <label htmlFor="taskDisc" >Task Discription</label>
+                <textarea rows='10' type="text" />
+                </div>
+                <div>
+                 <label htmlFor="dueDate" >Due Date</label>
+                <input type="Date" />
+                </div>
+                <div>
+                <label htmlFor="document" >Document</label>
+                <input type="file" />
+                </div>
+            <button type="submit">Submit</button>
             </form>
         </div>
     )
